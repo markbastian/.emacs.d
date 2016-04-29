6db18657971ef7c0ec7ea75c067d3d41dd918a33
Mark Bastian <markbastian@gmail.com>
2016-04-28 22:18:33 -0600

    Minor changes.

--- init.el
+++ init.el
@@ -148,3 +148,18 @@
 (load "setup-js.el")
 
 (global-set-key (kbd "RET") 'newline-and-indent)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(coffee-tab-width 2)
+ '(custom-safe-themes
+   (quote
+    ("9e54a6ac0051987b4296e9276eecc5dfb67fdcd620191ee553f40a9b6d943e78" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
