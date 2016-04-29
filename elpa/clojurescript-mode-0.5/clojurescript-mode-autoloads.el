58b647dfb40ee71038e5830a3563763c21f265db
Mark Bastian <markbastian@gmail.com>
2016-04-28 22:32:47 -0600

    Removed elpa.

--- elpa/clojurescript-mode-0.5/clojurescript-mode-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; clojurescript-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("clojurescript-mode.el") (22306 22477
-;;;;;;  692000 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; clojurescript-mode-autoloads.el ends here
